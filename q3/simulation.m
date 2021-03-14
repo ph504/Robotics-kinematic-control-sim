f216f829622eb05c4eae852f83c6c3e23d3dbc0c
Arya Parvizi <aresparvizi@gmail.com>
2021-03-14 16:38:40 +0330

    version after deadline, 1. p controller completed 2. bug fixed with the angles and other issues in the coefficients

--- q3/simulation.m
+++ q3/simulation.m
@@ -1,20 +1,25 @@
 clear
+close
 clc
 
 omega = pi/4;
 rotaion_matrix = [cos(omega) sin(omega) ; -sin(omega) cos(omega)];
 
-start_point = [60 0].';
-
+start_point = [5 0].';
+figure(1)
+hold on
+matrix = [];
 for i=1:8
     
-    figure(i)
-    main3([start_point;0])
-    start_point = rotaion_matrix * start_point
+    %figure(i)
+    
+    main3([0,0,0].',[start_point;0]);
+    start_point = rotaion_matrix * start_point;    
+    grid on
     
 end
-
-
+legend('1','2','3','4','5','6','7','8');
+hold off
 
 
 
